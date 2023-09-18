f68b2b9e16afe202019f9abe806f693394aeb5ef
YU <111847207+YU000jp@users.noreply.github.com>
2023-09-18 21:28:14 +0900

    feat(2 hop link): init

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import '@logseq/libs'; //https://plugins-doc.logseq.com/
 import { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user';
 //import { setup as l10nSetup, t } from "logseq-l10n"; //https://github.com/sethyuan/logseq-l10n
 //import ja from "./translations/ja.json";
+import { hopLinks } from "./hopLinks";
+import CSSfile from "./style.css?inline";
 
 
 /* main */
@@ -17,6 +19,23 @@ const main = () => {
   // })();
 
 
+  //ページ読み込み時に実行コールバック
+  logseq.App.onRouteChanged(async ({ template }) => {
+    if (template === '/page/:name') {
+      //2ホップリンク
+      if (!parent.document.getElementById("hopLinks")) hopLinks();
+    } //バグあり？onPageHeadActionsSlottedとともに動作保証が必要
+  });
+
+  //ページ読み込み時に実行コールバック
+  logseq.App.onPageHeadActionsSlotted(async () => {
+    //2ホップリンク
+    if (!parent.document.getElementById("hopLinks")) hopLinks();
+  }); //バグあり？onRouteChangedとともに動作保証が必要
+
+
+  logseq.provideStyle(CSSfile);
+
 };/* end_main */
 
 
@@ -24,8 +43,17 @@ const main = () => {
 /* user setting */
 // https://logseq.github.io/plugins/types/SettingSchemaDesc.html
 const settingsTemplate: SettingSchemaDesc[] = [
+  {//項目はなし
+    key: "heading000",
+    type: "heading",
+    title: "settings",
+    default: "",
+    description: "",
+  },
 
 ];
 
 
 logseq.ready(main).catch(console.error);
+
+
