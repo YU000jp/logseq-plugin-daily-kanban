e3ab2cb429de52666df8ca7f5c83fe01ccd2e6b9
YU <111847207+YU000jp@users.noreply.github.com>
2023-09-25 15:45:03 +0900

    feat(l10n): setup

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,20 @@
 import '@logseq/libs'; //https://plugins-doc.logseq.com/
-//import { setup as l10nSetup, t } from "logseq-l10n"; //https://github.com/sethyuan/logseq-l10n
-//import ja from "./translations/ja.json";
-import { settingsTemplate } from './settings';
 import { loadTwoHopLink } from './hopLinks';
-
+import { setup as l10nSetup } from "logseq-l10n"; //https://github.com/sethyuan/logseq-l10n
+import ja from "./translations/ja.json";
+import { settingsTemplate } from './settings';
 
 /* main */
-const main = () => {
-  // (async () => {
-  //   try {
-  //     await l10nSetup({ builtinTranslations: { ja } });
-  //   } finally {
-  /* user settings */
-  logseq.useSettingsSchema(settingsTemplate);
-  if (!logseq.settings) setTimeout(() => logseq.showSettingsUI(), 300);
-  //   }
-  // })();
+const main = async () => {
+  try {
+    await l10nSetup({ builtinTranslations: { ja } })
+  } finally {
+    /* user settings */
+    logseq.useSettingsSchema(settingsTemplate);
+    if (!logseq.settings) setTimeout(() => logseq.showSettingsUI(), 300);
+  }
 
   loadTwoHopLink();
-
 
   logseq.beforeunload(async () => {
     parent.document.getElementById("hopLinks")?.remove();
